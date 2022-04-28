326dbac03224ca29f4203792c2421b3277444dac
謝卉婷 <yuanningmo0511@gmail.com>
2022-04-28 19:50:14 +0800

    new HW1

---
+++ web410903864/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410903864.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HW1()
+        {
+            return View();
+        }
+
+        public ActionResult HW2()
+        {
+            return View();
+        }
+    }
+}
